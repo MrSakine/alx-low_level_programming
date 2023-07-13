8d1e826b7b178d6f99007144c591e2a41ca80a6c
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-13 16:23:39 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Fily M Sakine <msakine20@gmail.com>
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
+    puts("Programming is like building a multilingual puzzle");
+
     return (0);
 }
