a40fd8b25d41f6728e1a92fad259ae66cf6fed84
MrSakine <msakine20@gmail.com>
2023-07-13 09:29:22 -0700

    4-puts.c file update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 /*
  * File: 4-puts.c
- * Auth: Fily Mohamed Sakine
+ * Author: Fily M Sakine <msakine20@gmail.com>
  */
 
 #include <stdio.h>
@@ -13,7 +13,8 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+    puts("Programming is like building a multilingual puzzle");
 
-	return (0);
+    return (0);
 }
+
