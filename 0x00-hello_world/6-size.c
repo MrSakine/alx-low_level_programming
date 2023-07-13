188c12d6f843150c28388a96eeaab4d7b9bf160f
MrSakine <msakine20@gmail.com>
2023-07-13 09:50:41 -0700

    A program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+/*
+ * File: 6-size.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints size of various types on the computer it is compiled and run on
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long));
+printf("Size of a long long int: %lld byte(s)\n", sizeof(long));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -17,3 +17,6 @@ A program that prints exactly "Programming is like building a multilingual puzzl
 
 # 5-printf.c
 A program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line
+
+# 6-size.c
+A program that prints the size of various types on the computer it is compiled and run on
