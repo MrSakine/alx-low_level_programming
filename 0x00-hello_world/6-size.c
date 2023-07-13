902578bbb8f950e2453864dc330533cea6498a84
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-13 16:53:25 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of a int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 printf("Size of a long long int: %lld byte(s)\n", sizeof(long));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("Size of a float: %f byte(s)\n", sizeof(float));
 return (0);
 }
