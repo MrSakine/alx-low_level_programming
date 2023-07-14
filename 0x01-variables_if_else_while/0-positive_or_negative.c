b685c40ca3df53da9e40b292a4c8e09cb8a9da9b
MrSakine <msakine20@gmail.com>
2023-07-14 08:07:24 -0700

    A script to print whether the number stored in the variable n is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the number stored in the variable n is positive
+ *               or negative
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        }
+        else
+        {
+                printf("%d is negative\n", n);
+        }
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+0x01. C - Variables, if, else, while
+
+# 0-positive_or_negative.c
+A script to print whether the number stored in the variable n is positive or negative
