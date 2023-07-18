669b44ee509ede9a2fe6a86574be2b54f34618d1
MrSakine <msakine20@gmail.com>
2023-07-18 04:27:53 -0700

    Prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-putchar.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints _putchar
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+0x02. C - Functions, nested loops
+
+# _putchar 
+Prints _putchar, followed by a new line
