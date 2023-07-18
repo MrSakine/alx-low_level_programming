11d3b899587489e3f50ade75fd3b73c0b66ab8df
MrSakine <msakine20@gmail.com>
2023-07-18 11:10:16 -0700

    Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+/*
+ * File: 8-24_hours.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+* jack_bauer - every minute of the day of Jack Bauer
+* starting from 00:00 to 23:59
+*
+* Return: void
+*/
+
+void jack_bauer()
+{
+    int N = 23;
+    int M = 60;
+    int i;
+    int j;
+  
+    for (i = 0; i <= N; i++) {
+        for (j = 0; j < M; j++) {
+          _putchar((i / 10) + '0');
+          _putchar((i % 10) + '0');
+          _putchar(':');
+          _putchar((j / 10) + '0');
+          _putchar((j % 10) + '0');
+          _putchar('\n');
+        }
+    }
+    return;
+}
