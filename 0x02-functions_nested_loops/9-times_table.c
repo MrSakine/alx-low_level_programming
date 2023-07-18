ca7bc88f4e4c7b983796871080b4953adc48d7de
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-18 20:04:54 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,6 +33,7 @@ void times_table(void)
                 _putchar((y % 10) + '0');   
             }else{
                 _putchar((y % 10) + '0');
+                _putchar(' ');
             }
             
             if (z < N - 2) {
