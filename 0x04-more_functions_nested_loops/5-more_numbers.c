1a3b18d18d835815e7ad787ecc511f77ebb6fbdc
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-20 11:52:32 +0000

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,11 @@
 
 void more_numbers(void)
 {
-int n = 10;
-int m = 14;
 int i;
 int j;
-for (i = 0; i <= n; i++)
+for (i = 0; i <= 10; i++)
 {
-for (j = 0; j <= m; j++)
+for (j = 0; j <= 14; j++)
 {
 if (j > 9)
 {
