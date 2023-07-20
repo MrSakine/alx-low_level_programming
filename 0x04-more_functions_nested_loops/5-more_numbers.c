44d11185a0152c8a48cb4174f03b2b154f28cf21
MrSakine <msakine20@gmail.com>
2023-07-20 04:45:09 -0700

    more_numbers - Print 10 times the numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+/*
+ * File: 5-more_numbers.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * more_numbers - 10 times the numbers
+ * from 0 to 14, followed by a new line
+ *
+ * Return: void
+ */
+
+void more_numbers(void); {
+    int n = 10;
+    int m = 14;
+    
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (j > 9) {
+                putchar((j / 10) + '0');
+            }
+            putchar((j % 10) + '0');
+        }
+        putchar('\n');
+    }
+    return;
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -16,3 +16,7 @@ Print the numbers
 # 4-print_most_numbers.c
 Print the numbers
  * from 0 to 9, followed by a new line
+
+# 5-more_numbers.c
+Print 10 times the numbers
+ * from 0 to 14, followed by a new line
