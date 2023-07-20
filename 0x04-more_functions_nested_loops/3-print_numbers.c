4208504803f9073b927b6ef2bf159482104816a3
MrSakine <msakine20@gmail.com>
2023-07-20 04:03:25 -0700

    Print the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ * File: 3-print_numbers.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers
+ * from 0 to 9, followed by a new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void) {
+    int n = 9;
+    int i;
+    for (i = 0; i <= n; i++)
+    {
+        _putchar(i + '0');
+    }
+    _putchar('\n');
+    return;
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -8,3 +8,7 @@ Checks for a digit (0 through 9)
 
 # 2-mul.c
 multiplies two integers
+
+# 3-print_numbers.c
+Print the numbers
+ * from 0 to 9, followed by a new line
