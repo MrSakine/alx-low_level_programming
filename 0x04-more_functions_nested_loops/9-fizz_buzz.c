f5bb1d7fea18cc7bd097b20e978642ddc3fd2bc0
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-20 15:02:50 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,26 +14,32 @@
  * Return: Always 0.
  */
 
-int main() {
-    int N = 100;
-    int i;
-    
-    for (i = 1; i <= N; i++) {
-        if (i % 3 == 0) {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0) {
-            printf("Buzz");
-        }
-        else if ((i % 3 == 0) && (i % 5 == 0)) {
-            printf("FizzBuzz");
-        }
-        else{
-            printf("%d", i);
-        }
-        
-        putchar(' ');
-    }
-    putchar('\n');
-    return (0);
+int main()
+{
+int N = 100;
+int i;
+
+for (i = 1; i <= N; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else if (i % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d", i);
+}
+putchar(' ');
+}
+
+putchar('\n');
+return (0);
 }
