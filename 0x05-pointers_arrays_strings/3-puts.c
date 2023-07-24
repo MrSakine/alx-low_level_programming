01def104efba9f1300db13f32746cbe0b656b521
MrSakine <msakine20@gmail.com>
2023-07-24 09:53:42 -0700

    _puts - prints a string,

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+/*
+ * File: 3-puts.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _puts - prints a string,
+ * followed by a new line, to stdout
+ *
+ * @str: string to write
+ *
+ * Return: void.
+ */
+
+void _puts(char *str)
+{
+    int j;
+
+    for (j = 0; str[j] != '\0'; j++)
+    {
+        _putchar(str[j]);
+    }
+    
+    _putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -8,3 +8,6 @@ A function that swaps the values of two integers
 
 # 2-strlen.c
 A function that returns the length of a string
+
+# 3-puts.c
+A function that prints a string, followed by a new line, to stdout
