7a5208587b3dc74f65f796bbe39c5d3f247022c0
MrSakine <msakine20@gmail.com>
2023-07-24 09:28:52 -0700

    swap_int - swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-swap.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b) {
+    int c;
+
+    c = *a;
+    *a = *b;
+    *b = c;
+    
+    return;
+}
