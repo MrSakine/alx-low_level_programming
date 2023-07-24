b95d3a6f2f06ac5853642c672b38889d98309fa7
MrSakine <msakine20@gmail.com>
2023-07-24 12:18:27 -0700

    rev_string - reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+/*
+ * File: 5-rev_string.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to reverse
+ *
+ * Return: void.
+ */
+
+void rev_string(char *s)
+{
+        int j;
+        int i = 0;
+        char t;
+
+        for (j = 0; s[j] != '\0'; j++)
+        {
+                i += 1;
+        }
+
+        char c[i];
+
+        for (j = i - 1; j >= i / 2; j--)
+        {
+                t = s[j];
+                s[j] = s[i - j - 1];
+                s[i - j - 1] = t;
+        }
+}
