229d06174d6ae514d4a54efc3f36f00bb747def6
MrSakine <msakine20@gmail.com>
2023-07-24 10:10:50 -0700

    print_rev - prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+/*
+ * File: 4-print_rev.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * followed by a new line
+ *
+ * @s: string to reverse
+ *
+ * Return: void.
+ */
+
+void print_rev(char *s) {
+	int j;
+	int i = 0;
+
+	for (j = 0; s[j] != '\0'; j++) {
+	i += 1;
+	}
+
+	for (j = i - 1; s[j] != '\0'; j--) {
+	_putchar(s[j]);
+	}
+
+	_putchar('\n');
+
+    return;
+}
