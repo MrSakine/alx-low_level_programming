f88441650d9457776e0bbc01b6d7e8829d1bc1b4
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-24 17:11:30 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -19,11 +19,11 @@ void print_rev(char *s) {
 	int i = 0;
 
 	for (j = 0; s[j] != '\0'; j++) {
-	i += 1;
+		i += 1;
 	}
 
 	for (j = i - 1; s[j] != '\0'; j--) {
-	_putchar(s[j]);
+		_putchar(s[j]);
 	}
 
 	_putchar('\n');
