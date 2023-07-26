2bec8f343a82a805ef26d11e1603900aeaf0c997
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-26 17:05:56 +0000

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,19 +15,19 @@
 
 char *leet(char *a)
 {
-	char low[] = {'a', 'e', 'o', 't', 'l'};
-	char upper[] = {'A', 'E', 'O', 'T', 'L'};
-	char number[] = {4, 3, 0, 7, 1};
-	int i;
-	int j;
+char low[] = {'a', 'e', 'o', 't', 'l'};
+char upper[] = {'A', 'E', 'O', 'T', 'L'};
+char number[] = {4, 3, 0, 7, 1};
+int i;
+int j;
 
-	for (i = 0; a[i] != '\0'; i++) {
-		for (j = 0; j < 5; j++) {
-			if (a[i] == low[j] || a[i] == upper[j]) {
-				a[i] = number[j] + '0';
-			}
-		}
-	}
+for (i = 0; a[i] != '\0'; i++) {
+for (j = 0; j < 5; j++) {
+if (a[i] == low[j] || a[i] == upper[j]) {
+a[i] = number[j] + '0';
+}
+}
+}
 
-	return (a);
+return (a);
 }
