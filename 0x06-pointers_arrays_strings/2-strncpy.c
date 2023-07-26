0a2b2320101d11161042f1ff8b569b153f6180dd
MrSakine <msakine20@gmail.com>
2023-07-26 03:19:36 -0700

    _strncpy - copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+/*
+ * File: 2-strncpy.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: copy destination
+ * @src: copy source
+ * @n: bytes from @src
+ *
+ * Return: pointer to dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+        int i = 0;
+        int len = 0;
+
+        while (dest[i++])
+        {
+                len++;
+        }
+
+        for (i = 0; i < n && src[i] != '\0'; i++)
+        {
+                dest[i] = src[i];
+        }
+
+        for (; i < n; i++) {
+                dest[i] = '\0';
+        }
+
+        return (dest);
+}
