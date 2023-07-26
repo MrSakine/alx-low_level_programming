de4306fa478d2c7c5de073f885e1f1b09371a11d
MrSakine <msakine20@gmail.com>
2023-07-26 06:05:38 -0700

    reverse_array - reverses the content of

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+/*
+ * File: 4-rev_array.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of
+ * an array of integers
+ *
+ * @a: array
+ * @n: length of array
+
+ *
+ * Return: void.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int x;
+
+	for (i = n - 1; i >= n / 2; i--) {
+		x = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = x;
+	}
+}
