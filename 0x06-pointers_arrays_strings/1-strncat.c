ad1d87768e98992d1ff5a9a9ffd53963ebd57ac8
MrSakine <msakine20@gmail.com>
2023-07-26 03:11:37 -0700

    _strncat - concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+/*
+ * File: 1-strncat.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: copy destination
+ * @src: copy source
+ * @n: bytes from @src
+ *
+ * Return: pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+        int i = 0;
+        int len = 0;
+
+        while (dest[i++])
+        {
+                len++;
+        }
+
+        for (i = 0; i < n && src[i] != '\0'; i++)
+        {
+                dest[len + i] = src[i];
+        }
+
+        dest[len + i] = '\0';
+
+        return (dest);
+}
