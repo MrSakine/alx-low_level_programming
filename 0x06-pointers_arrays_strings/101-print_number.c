d7287e528e8920e590f0994728653d88ec441b4b
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-26 16:14:04 +0000

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,38 @@
+/*
+ * File: 101-print_number.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: integer to print
+ *
+ * Return: void.
+ */
+
+void print_number(int n)
+{
+	unsigned int num = 0;
+	int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	m = num;
+
+	if (num / 10) {
+		print_number(num / 10);
+	}
+
+	_putchar((m % 10) + '0');
+}
