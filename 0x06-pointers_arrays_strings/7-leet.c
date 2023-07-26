d8ef67750601421b2fa944d027de7a8a77f19e42
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-26 14:16:13 +0000

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+/*
+ * File: 7-leet.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ *
+ * @a: string to change
+ *
+ * Return: char*.
+ */
+
+char *leet(char *a)
+{
+	char low[] = {'a', 'e', 'o', 't', 'l'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
+	char number[] = {4, 3, 0, 7, 1};
+	int i;
+	int j;
+
+	for (i = 0; a[i] != '\0'; i++) {
+		for (j = 0; j < 5; j++) {
+			if (a[i] == low[j] || a[i] == upper[j]) {
+				a[i] = number[j] + '0';
+			}
+		}
+	}
+
+	return (a);
+}
