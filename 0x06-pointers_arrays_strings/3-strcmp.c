f939d4e494f985d907157362e3a361bdfca5ae02
MrSakine <msakine20@gmail.com>
2023-07-26 03:42:38 -0700

    _strcmp - compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+/*
+ * File: 3-strcmp.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: int.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+    int i;
+    int a = 0;
+    int b = 0;
+    int c;
+
+    for (i = 0; s1[i] != '\0'; i++)
+    {
+        a += (s1[i] - '0');
+    }
+
+    for (i = 0; s2[i] != '\0'; i++)
+    {
+        b += (s2[i] - '0');
+    }
+
+    return (a - b);
+}
