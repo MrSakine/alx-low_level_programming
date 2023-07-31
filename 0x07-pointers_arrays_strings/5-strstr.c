0c2aaa44fbe032491e1f08c3686f288bf7aa69e0
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-07-31 13:22:53 +0000

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,22 +18,14 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
 	int j = 0;
-	int len1 = 0;
-	int len2 = 0;
 
-	while (haystack[i++])
-		len1++;
-
-	while (needle[j++])
-		len2++;
-
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; needle[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; haystack[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i])
+			if (needle[i] == haystack[j])
 			{
-				return (haystack + (len1 - len2 + 1));
+				return (haystack + j);
 			}
 		}
 	}
