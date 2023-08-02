abc0884a5c01e2ff141d440321f5055386f53c76
MrSakine <msakine20@gmail.com>
2023-08-02 03:13:48 -0700

    _pow_recursion - returns the value

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+/*
+ * File: 4-pow_recursion.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of
+ * x raised to the power of y
+ *
+ * @x: int
+ * @y: int
+ *
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	return x * _pow_recursion(x, y - 1);
+}
