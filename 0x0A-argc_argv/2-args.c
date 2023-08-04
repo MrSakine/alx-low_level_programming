bf0b891d09f14ed5027070eaaa6b38d66e8fc3b0
MrSakine <msakine20@gmail.com>
2023-08-04 07:51:40 -0700

    main - Prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,32 @@
+/*
+ * File: 2-args.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) * argv[])
+{
+	int count;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	else
+	{
+		printf("The command had no other arguments.\n");
+	}
+
+	return (0);
+}
