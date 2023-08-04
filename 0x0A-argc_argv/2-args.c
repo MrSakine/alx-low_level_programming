83b3c9ed4b77b3890733c8c7a8f975e6d68cdbab
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-08-04 14:53:22 +0000

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,20 +12,13 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) * argv[])
 {
 	int count;
 
-	if (argc > 1)
+	for (count = 0; count < argc; count++)
 	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("%s\n", argv[count]);
-		}
-	}
-	else
-	{
-		printf("The command had no other arguments.\n");
+		printf("%s\n", argv[count]);
 	}
 
 	return (0);
