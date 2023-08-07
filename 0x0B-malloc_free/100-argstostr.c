2056c818c126b33353f3a1b2925bb1f726ec5f9f
MrSakine <msakine20@gmail.com>
2023-08-07 13:54:16 -0700

    argstostr - frees a 2 dimensional grid previously

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,60 @@
+/*
+ * File: 100-argstostr.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - frees a 2 dimensional grid previously
+ * created by your alloc_grid function
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: NULL if ac == 0 or av == NULL
+ * On success, a pointer to a new string,
+ * or NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int i;
+	int j;
+	int index = 0;
+	int c = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			c++;
+		}
+	}
+
+	s = malloc(c + 1 * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			s[index] = av[i][j];
+			index++;
+		}
+
+		s[index] = '\n';
+		index++;
+	}
+
+	s[c] = '\0';
+
+	return (s);
+}
