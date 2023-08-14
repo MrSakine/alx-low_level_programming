4c71a3de1a04a48c67f9af6e97faaf4a3250f70d
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-08-14 21:52:13 +0000

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,8 +3,8 @@
 
 /*
  * File: 0-object_like_macro.h
- * Auth: Fily Mohamed Sakine
- * Desc: a Hhader file that defines a macro named
+ * Author: Fily Mohamed Sakine
+ * Description: a header file that defines a macro named
  *       SIZE as an abbreviation for the token 1024.
  */
 
