484afed46a8f61faa1cb070fc3f62b751a16169d
MrSakine <msakine20@gmail.com>
2023-08-14 07:53:50 -0700

    init_dog - init a dog type structure.

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-init_dog.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+/**
+ * init_dog - init a dog type structure.
+ *
+ * @d: dog type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -3,4 +3,17 @@
 # dog.h
 Define a new type struct dog
 
-# 
+# 1-init_dog.c
+A function that initialize a variable of type struct dog
+
+# 2-print_dog.c
+A function that prints a struct dog
+
+# 3-
+Define a new type dog_t as a new name for the type struct dog
+
+# 4-new_dog.c
+A function that creates a new dog
+
+# 5--free_dog.c
+A function that frees dogs 
