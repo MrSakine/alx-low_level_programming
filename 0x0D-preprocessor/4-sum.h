dadf103dfd3fa5154f2cef595bf15d9727652d3f
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-08-14 21:47:06 +0000

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -4,7 +4,8 @@
 /*
  * File: 4-sum.h
  * Auth: Fily Mohamed Sakine
- * Desc: a function-like macro SUM(x, y) that computes the sum of the numbers x and y
+ * Desc: a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y
  */
 
 #define SUM(x, y) ((x) + (y))
