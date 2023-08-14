48f6b6f7ba44e9c9da4cab5732b28951a9d11b30
MrSakine <msakine20@gmail.com>
2023-08-14 07:41:33 -0700

    initial commit 0x0E

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+0x0E. C - Structures, typedef
+
+# dog.h
+Define a new type struct dog
+
+# 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+/*
+ * File: dog.h
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+/**
+ * struct dog - dog type structure.
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: A dog type structure with
+ * the following attributes:
+ * name, age and owner
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
