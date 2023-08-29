096cee099b66d778e73f1ed8e53135086251d357
MrSakine <msakine20@gmail.com>
2023-08-29 10:50:23 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+i/*
+ * File: 0-print_list.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: singly linked list
+ *
+ * Return: length of element of singly linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+    size_t len = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        len++;
+    }
+
+    return (len);
+}
+
