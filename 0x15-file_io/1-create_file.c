94c0734b13e8a46b5b6ea44c3f87ed9dda2be8d8
MrSakine <msakine20@gmail.com>
2023-09-04 08:09:43 -0700

    create_file - creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+/*
+ * File: 1-create_file.c
+ * Author: Fily M Sakine <msakine20@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file name in string
+ * @letters: file content
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t op;
+	ssize_t wr;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[len])
+		len++;
+
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
+
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -2,3 +2,6 @@
 
 # 0-read_textfile.c
 A function that reads a text file and prints it to the POSIX standard output
+
+# 1-create_file.c
+A function that creates a file
