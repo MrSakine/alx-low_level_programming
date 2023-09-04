04da3bf0db48c606636bc294cc4c3ad4abb9c102
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-09-04 15:13:45 +0000

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[len])
-		len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
 	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
@@ -34,4 +37,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
