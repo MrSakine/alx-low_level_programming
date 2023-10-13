06f59626078ba1ceac88028f6e5cad35fd9b8301
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-13 06:02:53 +0000

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,8 +12,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *last, *new;
 	unsigned int len = 0;
 
-	if (h == NULL || *h == NULL)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
