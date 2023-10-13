509db126fa41386da12106e858620bbd40c4e3f5
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-13 06:56:01 +0000

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (-1);
 		temp = last->next;
 		last->next = temp->next;
-		temp->prev = NULL;
+		temp->prev = last;
 		last->next->next->prev = last;
 		free(temp);
 	}
