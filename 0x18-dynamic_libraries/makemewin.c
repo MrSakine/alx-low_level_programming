3e44acbb0373aa7760058384a1f1b402363ab597
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-16 21:32:37 +0000

    Update makemewin.c

--- 0x18-dynamic_libraries/makemewin.c
+++ 0x18-dynamic_libraries/makemewin.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+/**
+ * puts - prints a string,
+ * followed by a new line, to stdout
+ *
+ * @s: string to write
+ *
+ * Return: number of characters printed
+ */
 int puts(const char *s)
 {
-  return printf("--> Please make me win!\n");
+	return (printf("--> Please make me win!\n"));
 }
