114d8bd4e993ab3771b873437c4b6d47acc18c32
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-17 17:26:30 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -fPIC -ldl makemewin.c -o makemewin.so
-LD_PRELOAD="./makemewin.so" ./gm 9 8 10 24 75 9
+wget -P /tmp https://github.com/Pronothurah/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
