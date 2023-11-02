a8d13abd90cbe9c0b9fcc36550ef3a38f04ce97e
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-02 22:39:46 +0000

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,6 +36,15 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+int check_for_collision(hash_table_t *ht, hash_node_t *node);
+void move_to_top(hash_table_t *ht, hash_node_t *node, unsigned long int kfn);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+void free_hash_node(hash_node_t *node);
+void free_hash_table(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
-
