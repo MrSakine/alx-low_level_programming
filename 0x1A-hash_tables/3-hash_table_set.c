9cb920d0e9aff170d63a79be77ba424407bc1a02
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-03 02:21:56 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -78,7 +78,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || ht->array == NULL)
 		return (0);
-	if (key == NULL || strcmp(key, "") == 0 || value == NULL)
+	if (strcmp(key, "") == 0)
 		return (0);
 	if (is_whitespace_string(key) || is_whitespace_string(value))
 		return (0);
