7eefbe293a53dc089a2b395039795c6902aeb297
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-04 01:16:35 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -60,6 +60,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (0);
 
+	if (ht->array == NULL)
+		return (0);
+
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
